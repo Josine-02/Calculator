97ea977462537c652346158fb72f507212b02042
Josine-02 <josine.deleeuwvanweenen@gmail.com>
2025-03-08 21:01:30 +0100

    set up documents

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const title = document.createElement("div")
+title.classList.add("title")
+title.textContent = "Calculator"
+const container= document.querySelector(".container")
+container.appendChild(title)
