a3fee4498e7a18c49f1915d9d4db3097837ee5ff
Josine-02 <josine.deleeuwvanweenen@gmail.com>
2025-03-08 21:08:19 +0100

    add math functions

--- javascript.js
+++ javascript.js
@@ -3,3 +3,28 @@ title.classList.add("title")
 title.textContent = "Calculator"
 const container= document.querySelector(".container")
 container.appendChild(title)
+
+function add(a,b){
+    return a+b
+}
+
+function substract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+
+let a = 5
+let b=2
+
+console.log(add(a,b))
+console.log(substract(a,b))
+console.log(divide(a,b))
+console.log(multiply(a,b))
